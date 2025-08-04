dd5bd42750d4d581c5e696b7295dfd3209a7f866
Joshua Vidale <JoshuaVidale@gmail.com>
2025-08-04 02:01:36 -0400

    first commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,11 +4,12 @@ import {
   registerUser,
   getMe,
 } from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 
-router.get("/me", getMe);
+router.get("/me", protect, getMe);
 
 export default router;
