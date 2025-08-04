1c3947ad2375b541421aa715bb269efcfe745a14
Joshua Vidale <JoshuaVidale@gmail.com>
2025-08-04 01:13:10 -0400

    commit

--- server.js
+++ server.js
@@ -60,4 +60,7 @@ app.use(errorHandler);
 
 // Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
